74312163ef3bac8b1d7e0ce3d19f4de19857c9e5
Roopus-V <60544941+Roopus-V@users.noreply.github.com>
2025-07-22 21:37:40 +0530

    Update database.sql
    Added Create DATABASE

--- database.sql
+++ database.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `movies`
 --
-
+CREATE DATABASE IF NOT EXISTS moviedb;
+USE moviedb;
 DROP TABLE IF EXISTS `movies`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
